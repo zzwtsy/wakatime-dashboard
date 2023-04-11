eaf5de83bb16562df4ee42a8ed957658f7373677
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2023-04-11 22:43:42 +0800

    优化代码

--- src/ts/service/GetPieDataAndShow.ts
+++ src/ts/service/GetPieDataAndShow.ts
@@ -13,50 +13,63 @@ export const getPieDataAndShow = async (gistId: string) => {
   store.echartsLoading = true;
   try {
     // 获取 Gist 中所有符合条件的文件的 raw url
-    const urls = await Api.getRawUrl(gistId);
+    const urls = await Api.getRawUrl(gistId).catch((err) => {
+      console.error("获取 RawURLs 失败：", err);
+      throw new Error("获取 RawURLs 失败");
+    });
     const resList = await Api.getGistPostsContent(
       urls.reverse().slice(0, store.selectValue)
-    );
-    // 获取饼图数据
-    const pieData = await Parse.parsePieData(resList);
+    ).catch((err) => {
+      console.error("获取 Gist 内容失败：", err);
+      throw new Error("获取 Gist 内容失败");
+    });
     // 获取柱状图数据
     const barData = Parse.parseBarData(resList);
-    // 解析[编程语言|电脑|代码编辑器|操作系统]的时长
-    const languages = pieData.get(DataType.Languages);
-    const machines = pieData.get(DataType.Machines);
-    const editors = pieData.get(DataType.Editors);
-    const operatingSystems = pieData.get(DataType.OperatingSystems);
-
-    // 更新展示组件
+    // 更新柱状图展示组件
     store.projectsOption = ChartsOptions.setBarChartsData(
       barData.xAxisData,
       barData.seriesData,
       "Projects Code Time"
     );
-    if (languages) {
-      store.languagePieChartOption = ChartsOptions.setPieChartsData(
-        languages,
-        "Language Usage Time"
-      );
-    }
-    if (machines) {
-      store.machinesOption = ChartsOptions.setPieChartsData(
-        machines,
-        "Machines"
-      );
-    }
-    if (editors) {
-      store.editorsOption = ChartsOptions.setPieChartsData(editors, "Editors");
-    }
-    if (operatingSystems) {
-      store.operatingSystemsOption = ChartsOptions.setPieChartsData(
-        operatingSystems,
-        "Operating Systems"
-      );
-    }
+
+    const chartsDataList = [
+      {
+        key: DataType.Languages,
+        title: "Language Usage Time",
+        storeKey: "languagePieChartOption",
+      },
+      { key: DataType.Machines, title: "Machines", storeKey: "machinesOption" },
+      { key: DataType.Editors, title: "Editors", storeKey: "editorsOption" },
+      {
+        key: DataType.OperatingSystems,
+        title: "Operating Systems",
+        storeKey: "operatingSystemsOption",
+      },
+    ];
+
+    // 获取饼图数据
+    const pieData = await Parse.parsePieData(resList);
+
+    // 更新饼图展示组件
+    chartsDataList.forEach((chartData) => {
+      const data = pieData.get(DataType.OperatingSystems);
+
+      if (data) {
+        const storeKey = chartData.storeKey;
+        store[storeKey] = ChartsOptions.setPieChartsData(data, chartData.title);
+      }
+    });
   } catch (e: any) {
-    console.error(e);
-    alert(`获取数据失败：${e.message}`);
+    // 记录异常上下文信息
+    const context = {
+      type: "fetch data error",
+      function: "getPieDataAndShow",
+      api: "Api.getRawUrl or Api.getGistPostsContent or Parse.parsePieData",
+      gistId,
+      message: e.message,
+      stack: e.stack,
+    };
+    console.error(context);
     throw new Error("获取数据失败");
   } finally {
     // 关闭 echarts loading 动画
