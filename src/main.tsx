751ce76f490f8532e59b494e354d8096e8f69639
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2024-01-18 20:44:59 +0800

    设置默认主题为系统主题

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "./components/ThemeProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
     <App />
     </ThemeProvider>
   </React.StrictMode>
